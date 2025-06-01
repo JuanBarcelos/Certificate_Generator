81de55d013c9fc9e96a8cc48e5fa77d2717ee864
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-05-31 22:46:16 -0300

    finalizado estrutura inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { BaseUIComponent } from "./components/base-ui/base-ui.component";
 import { CertificadosComponent } from "./pages/certificados/certificados.component";
 import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
+import { CertificadoComponent } from "./pages/certificado/certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, BaseUIComponent, CertificadosComponent, CertificadoFormComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUIComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
