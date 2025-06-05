4de8d2f876111d237fe83241f2dde94ed325e63d
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-06-05 16:32:14 -0300

    manipulação de formulário

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
 import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
+import { FormsModule, type NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import type { Certificate } from '../../interfaces/certificate';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.scss'
 })
 export class CertificadoFormComponent {
 
+  certificate: Certificate = {
+    name: '',
+    listActivities: []
+  }
+  activity: string = '';
+
+  invalidFields(field: NgModel) {
+    return field.invalid && field.touched
+  }
+  validForm() {
+    return this.certificate.name.length > 0 && this.certificate.listActivities.length > 0
+  }
+  addActivity() {
+    if (this.activity.length > 0) {
+      this.certificate.listActivities.push(this.activity)
+      this.activity = ''
+    }
+  }
+  removeActivity(index: number) {
+    this.certificate.listActivities.splice(index, 1)
+  }
+  generateCertificate() {
+    if (!this.validForm()) {
+      alert('Preencha todos os campos')
+      return
+    }
+    console.log(this.certificate)
+  }
 }
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { ItemCertificadoComponent } from "../../components/item-certificado/item-certificado.component";
+import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-certificados',
-  imports: [ItemCertificadoComponent],
+  imports: [ItemCertificadoComponent, SecondaryButtonComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.scss'
 })
