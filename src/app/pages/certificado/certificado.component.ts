52c697225e6a788f7fa0c8779ea1393ae310b98b
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-06-14 11:24:05 -0300

    aprendendo a fazer download e sobre a biblioteca html2canvas

--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SecondaryButtonComponent } from "../../components/secondary-button/secondary-button.component";
 import { ActivatedRoute } from '@angular/router';
 import { CertificateService } from '../../services/certificate.service';
-import type { Certificate } from '../../interfaces/certificate';
+import { Certificate } from '../../interfaces/certificate';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-certificado',
@@ -12,12 +13,26 @@ import type { Certificate } from '../../interfaces/certificate';
 })
 export class CertificadoComponent implements OnInit {
   certificate: Certificate | undefined;
+  @ViewChild('certificateContainer') certificateElement!: ElementRef;
   constructor(private router: ActivatedRoute, private certificateService: CertificateService) { }
 
   ngOnInit(): void {
     this.router.params.subscribe(params => {
       const id = params['id'];
       this.certificate = this.certificateService.getCertificateById(id);
+    });
+  }
+
+  downloadCertificate() {
+    if (!this.certificate) {
+      console.error('Certificado não encontrado.');
+      return;
+    }
+    html2canvas(this.certificateElement.nativeElement, {scale: 2}).then(canvas => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'certificado_'+this.certificate?.name.replaceAll(' ','_')+'.png';
+      link.click();
     });
   }
 }
